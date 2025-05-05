e0a600574136e09296264c364c7ddb77effe4620
BalaAyyappan1 <balaayyappan11@gmail.com>
2025-05-05 22:08:43 +0530

    Imagekit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import AnimatedText from '@/components/AnimatedText';
+
 
 const page = () => {
   return (
     <div>
-        <AnimatedText />
+      hello
       
     </div>
   )
