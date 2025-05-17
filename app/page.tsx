b479fdbab927f4d90955febcde269263f321d688
BalaAyyappan1 <balaayyappan11@gmail.com>
2025-05-17 12:54:31 +0530

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 
 import Hero from '@/components/hero'
 
-import VideoAnimation from '@/components/VideoAnimation'
+
 export default function Home() {
   return (
     <div className="overflow-x-hidden">
-<VideoAnimation />
+
       <Hero />
     </div>
   );
